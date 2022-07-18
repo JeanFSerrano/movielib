b947676bacc14021604465161415441482badf95
Jean de Freitas Serrano <bountyann4@gmail.com>
2022-07-18 15:40:19 -0300

    testando mais algumas alterações pois o vercel ainda não reconheceu o poster_path

--- src/Components/MovieCard.jsx
+++ src/Components/MovieCard.jsx
@@ -1,17 +1,25 @@
 
 import { FaStar } from 'react-icons/fa'
 import { Link } from 'react-router-dom';
+import { ImSpinner } from 'react-icons/im'
 
 import './MovieCard.css'
 
 
 const MovieCard = ({ movie, showLink = true, onClick }) => {
 
+    const moviePoster = movie.poster_path
+
     return (
 
         <div className='movie-container' onClick={onClick}>
+
+            {movie.length === 0 && (
+                <div><ImSpinner /></div>
+            )}
+
             <img
-                src={'https://image.tmdb.org/t/p/w500/' + movie.poster_path}
+                src={`https://image.tmdb.org/t/p/w500/${moviePoster}`}
                 alt={movie.name}
 
             />
@@ -20,7 +28,6 @@ const MovieCard = ({ movie, showLink = true, onClick }) => {
                 Nota: <FaStar /> {movie.vote_average}
             </p>
             {showLink && <Link to={`/movie/${movie.id}`}>Detalhes</Link>}
-
 
         </div>
 
